bf9ac7d0ba9b740189f3db48776dca86a55f17de
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-05-10 01:08:20 +0700

    Add widget of text and texfield for the description with the validation

--- lib/views/registrasi_screen.dart
+++ lib/views/registrasi_screen.dart
@@ -67,6 +67,26 @@ class _FormKulinerState extends State<FormKuliner> {
             controller: _nama,
             validator: _validateText,
           ),
+          SizedBox(height: 16),
+          Text(
+            "Deskripsi",
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 8),
+          TextFormField(
+            maxLines: 1,
+            decoration: InputDecoration(
+              hintText: "Masukkan Deskripsi",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            controller: _deskripsi,
+            validator: _validateText,
+          ),
         ],
       ),
     );
