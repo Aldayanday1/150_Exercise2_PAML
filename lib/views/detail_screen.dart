581f458a80956eea8b469794e74c5e98b44896de
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-05-09 23:26:44 +0700

    Add Class of Detail Screen and give a navigation from HomeView

---
+++ lib/views/detail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:kulinerjogja/model/kuliner.dart';
+
+class DetailView extends StatefulWidget {
+  final Kuliner kuliner;
+
+  const DetailView({super.key, required this.kuliner});
+
+  @override
+  State<DetailView> createState() => _DetailViewState();
+}
+
+class _DetailViewState extends State<DetailView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kulinerjogja/controllers/kuliner_controller.dart';
 import 'package:kulinerjogja/model/kuliner.dart';
+import 'package:kulinerjogja/views/detail_screen.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -32,82 +33,93 @@ class _HomeViewState extends State<HomeView> {
           title: Text("Kuliner Jogja", style: GoogleFonts.openSans()),
           toolbarHeight: 80,
         ),
-        body: Padding(
-          padding: EdgeInsets.only(top: 15),
-          child: SingleChildScrollView(
-            child: FutureBuilder<List<Kuliner>>(
-              future: _controller.getAllKuliner(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text("Error: ${snapshot.error}"));
-                } else {
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: snapshot.data?.length ?? 0,
-                    itemBuilder: (context, index) {
-                      Kuliner kuliner = snapshot.data![index];
-                      return InkWell(
-                        onTap: () {},
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 4, horizontal: 20),
-                          child: Card(
-                            color: Color.fromARGB(255, 255, 252, 244),
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Row(
-                                children: [
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(10),
-                                    child: Image.network(
-                                      kuliner.gambar,
-                                      width: 100.0,
-                                      height: 100.0,
-                                      fit: BoxFit.cover,
+        body: RefreshIndicator(
+          onRefresh: _refreshData,
+          child: Padding(
+            padding: EdgeInsets.only(top: 15),
+            child: SingleChildScrollView(
+              child: FutureBuilder<List<Kuliner>>(
+                future: _controller.getAllKuliner(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text("Error: ${snapshot.error}"));
+                  } else {
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: snapshot.data?.length ?? 0,
+                      itemBuilder: (context, index) {
+                        Kuliner kuliner = snapshot.data![index];
+                        return InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    DetailView(kuliner: kuliner),
+                              ),
+                            );
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 4, horizontal: 20),
+                            child: Card(
+                              color: Color.fromARGB(255, 255, 252, 244),
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Row(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: Image.network(
+                                        kuliner.gambar,
+                                        width: 100.0,
+                                        height: 100.0,
+                                        fit: BoxFit.cover,
+                                      ),
                                     ),
-                                  ),
-                                  const SizedBox(width: 16.0),
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          kuliner.nama,
-                                          style: GoogleFonts.roboto(
-                                            fontSize: 16.0,
-                                            color:
-                                                Color.fromARGB(255, 66, 66, 66),
-                                            fontWeight: FontWeight.bold,
+                                    const SizedBox(width: 16.0),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            kuliner.nama,
+                                            style: GoogleFonts.roboto(
+                                              fontSize: 16.0,
+                                              color: Color.fromARGB(
+                                                  255, 66, 66, 66),
+                                              fontWeight: FontWeight.bold,
+                                            ),
                                           ),
-                                        ),
-                                        SizedBox(
-                                          height: 7,
-                                        ),
-                                        Text(
-                                          kuliner.alamat,
-                                          style: GoogleFonts.roboto(
-                                            fontSize: 12.0,
-                                            color:
-                                                Color.fromARGB(255, 66, 66, 66),
+                                          SizedBox(
+                                            height: 7,
                                           ),
-                                        )
-                                      ],
+                                          Text(
+                                            kuliner.alamat,
+                                            style: GoogleFonts.roboto(
+                                              fontSize: 12.0,
+                                              color: Color.fromARGB(
+                                                  255, 66, 66, 66),
+                                            ),
+                                          )
+                                        ],
+                                      ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      );
-                    },
-                  );
-                }
-              },
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
             ),
           ),
         ));
