f0f67e7aa0632952f91d0f53346d87bac50100b2
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-05-09 23:42:37 +0700

    Changes and adding factory method in Kuliner model for JSON parsing

--- lib/model/kuliner.dart
+++ lib/model/kuliner.dart
@@ -1,83 +1,25 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:convert';
-
 class Kuliner {
   int id;
   String nama;
   String alamat;
   String gambar;
   String deskripsi;
+
   Kuliner({
     required this.id,
     required this.nama,
     required this.alamat,
-    required this.gambar,
     required this.deskripsi,
+    required this.gambar,
   });
 
-  Kuliner copyWith({
-    int? id,
-    String? nama,
-    String? alamat,
-    String? gambar,
-    String? deskripsi,
-  }) {
+  factory Kuliner.fromJson(Map<String, dynamic> json) {
     return Kuliner(
-      id: id ?? this.id,
-      nama: nama ?? this.nama,
-      alamat: alamat ?? this.alamat,
-      gambar: gambar ?? this.gambar,
-      deskripsi: deskripsi ?? this.deskripsi,
+      id: json['id'],
+      nama: json['nama'],
+      alamat: json['alamat'],
+      gambar: json['gambar'],
+      deskripsi: json['deskripsi'],
     );
-  }
-
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'id': id,
-      'nama': nama,
-      'alamat': alamat,
-      'gambar': gambar,
-      'deskripsi': deskripsi,
-    };
-  }
-
-  factory Kuliner.fromMap(Map<String, dynamic> map) {
-    return Kuliner(
-      id: map['id'] as int,
-      nama: map['nama'] as String,
-      alamat: map['alamat'] as String,
-      gambar: map['gambar'] as String,
-      deskripsi: map['deskripsi'] as String,
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory Kuliner.fromJson(String source) =>
-      Kuliner.fromMap(json.decode(source) as Map<String, dynamic>);
-
-  @override
-  String toString() {
-    return 'Kuliner(id: $id, nama: $nama, alamat: $alamat, gambar: $gambar, deskripsi: $deskripsi)';
-  }
-
-  @override
-  bool operator ==(covariant Kuliner other) {
-    if (identical(this, other)) return true;
-
-    return other.id == id &&
-        other.nama == nama &&
-        other.alamat == alamat &&
-        other.gambar == gambar &&
-        other.deskripsi == deskripsi;
-  }
-
-  @override
-  int get hashCode {
-    return id.hashCode ^
-        nama.hashCode ^
-        alamat.hashCode ^
-        gambar.hashCode ^
-        deskripsi.hashCode;
   }
 }
