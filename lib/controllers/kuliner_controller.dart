66f199b6e2ba289395f6b9336cd9c6991814223c
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-05-09 14:39:58 +0700

    Add Functionality to create data kuliner entries with addKuliner

--- lib/controllers/kuliner_controller.dart
+++ lib/controllers/kuliner_controller.dart
@@ -1,6 +1,12 @@
 class KulinerController {
-  
+
   final KulinerService kulinerService = KulinerService();
 
-
+  Future<Map<String, dynamic>> addKuliner(Kuliner kuliner, File? file) async {
+    Map<String, String> data = {
+      'nama': kuliner.nama,
+      'alamat': kuliner.alamat,
+      'deskripsi': kuliner.deskripsi
+    };
+  }
 }
