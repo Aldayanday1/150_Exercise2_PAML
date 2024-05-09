fe53225e395dc0de12abc7e9bdf6e24e552767f0
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-05-10 02:10:05 +0700

    Add widget appBar for Scaffold in detail screem

--- lib/views/detail_screen.dart
+++ lib/views/detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kulinerjogja/controllers/kuliner_controller.dart';
 import 'package:kulinerjogja/model/kuliner.dart';
 
 class DetailView extends StatefulWidget {
@@ -11,8 +12,15 @@ class DetailView extends StatefulWidget {
 }
 
 class _DetailViewState extends State<DetailView> {
+  final KulinerController _controller = KulinerController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 61, 61, 61),
+      appBar: AppBar(
+        title: Text("Detail Kuliner"),
+      ),
+    );
   }
 }
