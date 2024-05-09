218ddf9f2db68536d2da0e4d989ea3f98907b160
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-05-10 02:18:19 +0700

    Displays the text of the received parameters nama, alamat, deskripsi

--- lib/views/detail_screen.dart
+++ lib/views/detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kulinerjogja/controllers/kuliner_controller.dart';
 import 'package:kulinerjogja/model/kuliner.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DetailView extends StatefulWidget {
   final Kuliner kuliner;
@@ -20,6 +21,36 @@ class _DetailViewState extends State<DetailView> {
       backgroundColor: const Color.fromARGB(255, 61, 61, 61),
       appBar: AppBar(
         title: Text("Detail Kuliner"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.kuliner.nama,
+            style: GoogleFonts.roboto(
+              fontSize: 45.0,
+              color: Color.fromARGB(255, 66, 66, 66),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 30),
+          Text(
+            widget.kuliner.alamat,
+            style: GoogleFonts.roboto(
+              fontSize: 14.0,
+              color: Color.fromARGB(255, 66, 66, 66),
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            widget.kuliner.deskripsi,
+            style: GoogleFonts.roboto(
+              fontSize: 14.0,
+              color: Color.fromARGB(255, 66, 66, 66),
+            ),
+          ),
+          SizedBox(height: 10),
+        ],
       ),
     );
   }
