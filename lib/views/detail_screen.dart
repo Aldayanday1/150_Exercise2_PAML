48c4cae26f99a32602c223984819dc8181f1078d
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-05-10 02:25:27 +0700

    Add widget Column with the SingleChildScrollView to wrap the Card

--- lib/views/detail_screen.dart
+++ lib/views/detail_screen.dart
@@ -22,43 +22,58 @@ class _DetailViewState extends State<DetailView> {
       appBar: AppBar(
         title: Text("Detail Kuliner"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Card(
-          color: Color.fromARGB(255, 255, 252, 244),
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.kuliner.nama,
-                  style: GoogleFonts.roboto(
-                    fontSize: 45.0,
-                    color: Color.fromARGB(255, 66, 66, 66),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 30),
-                Text(
-                  widget.kuliner.alamat,
-                  style: GoogleFonts.roboto(
-                    fontSize: 14.0,
-                    color: Color.fromARGB(255, 66, 66, 66),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  widget.kuliner.deskripsi,
-                  style: GoogleFonts.roboto(
-                    fontSize: 14.0,
-                    color: Color.fromARGB(255, 66, 66, 66),
-                  ),
-                ),
-                SizedBox(height: 10),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Hero(
+              tag: 'image${widget.kuliner.id}',
+              child: Image.network(
+                widget.kuliner.gambar,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Card(
+                color: Color.fromARGB(255, 255, 252, 244),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.kuliner.nama,
+                        style: GoogleFonts.roboto(
+                          fontSize: 45.0,
+                          color: Color.fromARGB(255, 66, 66, 66),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      Text(
+                        widget.kuliner.alamat,
+                        style: GoogleFonts.roboto(
+                          fontSize: 14.0,
+                          color: Color.fromARGB(255, 66, 66, 66),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        widget.kuliner.deskripsi,
+                        style: GoogleFonts.roboto(
+                          fontSize: 14.0,
+                          color: Color.fromARGB(255, 66, 66, 66),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
