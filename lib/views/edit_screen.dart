503513613449a78c95526146f81f73ceabf4d315
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-05-10 03:37:23 +0700

    Add some Variable for TextController and add input field for name with label

--- lib/views/edit_screen.dart
+++ lib/views/edit_screen.dart
@@ -8,12 +8,46 @@ class EditKuliner extends StatefulWidget {
 }
 
 class _EditKulinerState extends State<EditKuliner> {
+  final _formKey = GlobalKey<FormState>();
+  final _nama = TextEditingController();
+  final _deskripsi = TextEditingController();
+
+  String? _validateText(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Kolom ini tidak boleh kosong';
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 61, 61, 61),
       appBar: AppBar(
         title: Text("Edit Screen"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Nama",
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 8),
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: "Masukkan Nama",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            controller: _nama,
+            validator: _validateText,
+          ),
+        ],
       ),
     );
   }
