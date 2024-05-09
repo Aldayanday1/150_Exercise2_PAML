4a1c96a079a9088d6ff45a535dd7f77e396767f6
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-05-09 14:43:43 +0700

    Add Handle Response in add kuliner for success and error messages

--- lib/controllers/kuliner_controller.dart
+++ lib/controllers/kuliner_controller.dart
@@ -8,5 +8,35 @@ class KulinerController {
       'alamat': kuliner.alamat,
       'deskripsi': kuliner.deskripsi
     };
+
+    try {
+      var response = await kulinerService.tambahKuliner(data, file);
+
+      if (response.statusCode == 200) {
+        return {
+          'success': true,
+          'message': 'Data berhasil disimpan',
+        };
+      } else {
+        if (response.headers['content-type']!.contains('application/json')) {
+          var decodedJson = jsonDecode(response.body);
+          return {
+            'success': false,
+            'message': decodedJson['message'] ?? 'Terjadi kesalahan',
+          };
+        }
+        var decodedJson = jsonDecode(response.body);
+        return {
+          'success': false,
+          'message':
+              decodedJson['message'] ?? 'Terjadi kesalahan saat menyimpan data',
+        };
+      }
+    } catch (e) {
+      return {
+        'success': false,
+        'message': 'Terjadi kesalahan: $e',
+      };
+    }
   }
 }
