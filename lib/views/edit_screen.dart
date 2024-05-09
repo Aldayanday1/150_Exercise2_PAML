89ba573a1a8cc7c33f2f119a1dd80bbe35d27dad
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-05-10 04:13:53 +0700

    Add gambar section with display of selected image

--- lib/views/edit_screen.dart
+++ lib/views/edit_screen.dart
@@ -133,6 +133,44 @@ class _EditKulinerState extends State<EditKuliner> {
                             ? const Text('Pilih Alamat')
                             : const Text('Ubah Alamat'),
                       ),
+                      SizedBox(height: 16),
+                      Text(
+                        "Gambar",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Container(
+                        height: 150,
+                        child: _image == null
+                            ? Text(
+                                "Tidak ada gambar yang dipilih!",
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 192, 95, 95),
+                                ),
+                              )
+                            : Container(
+                                width: 300,
+                                child: Card(
+                                  elevation: 10,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  clipBehavior: Clip.antiAlias,
+                                  child: _image!.path.startsWith('http')
+                                      ? Image.network(
+                                          _image!.path,
+                                          fit: BoxFit.cover,
+                                        )
+                                      : Image.file(
+                                          _image!,
+                                          fit: BoxFit.cover,
+                                        ),
+                                ),
+                              ),
+                      ),
                     ],
                   ),
                 ],
