8edebba764245c06ec9b2b3f15f24e6bf25867f0
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-05-09 22:54:48 +0700

    Add widget appBar and Body of Scaffold in home screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -10,6 +11,13 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 61, 61, 61),
+      appBar: AppBar(
+        title: Text("Kuliner Jogja", style: GoogleFonts.openSans()),
+        toolbarHeight: 80,
+      ),
+      body: ,
+    );
   }
 }
