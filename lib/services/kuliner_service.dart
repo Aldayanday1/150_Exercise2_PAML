f582c61d05269f5e348d27108740a69d31ffa0e5
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-05-09 13:35:34 +0700

    Add Function TambahKuliner for uploading new data and including optional some image

--- lib/services/kuliner_service.dart
+++ lib/services/kuliner_service.dart
@@ -1,7 +1,15 @@
 class class KulinerService {
-  final String baseUrl = 'http://192.168.56.1:8080/kuliner_150/';
 
+  final String baseUrl = 'http://192.168.56.1:8080/kuliner_150/';
   Uri getUri(String path) {
     return Uri.parse("$baseUrl$path");
+  }
+
+  Future<http.Response> tambahKuliner(
+      Map<String, String> data, File? file) async {
+    var request = http.MultipartRequest(
+      'POST',
+      getUri('add'),
+    );
   }
 }
