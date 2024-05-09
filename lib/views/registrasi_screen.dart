63688e76b4337c6617cc8c849a7687f8e7ab76c3
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-05-09 23:40:32 +0700

    Add Widget of FloatingActionButton and create a new Class of Registrasi Screen

---
+++ lib/views/registrasi_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FormKuliner extends StatefulWidget {
+  const FormKuliner({super.key});
+
+  @override
+  State<FormKuliner> createState() => _FormKulinerState();
+}
+
+class _FormKulinerState extends State<FormKuliner> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
